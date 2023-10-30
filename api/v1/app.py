e138950070c0c896df32160da7b669ac74333e1d
Aibueku O. Precious <aibuekuprecious@gmail.com>
2023-10-30 14:24:00 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,11 +6,11 @@ from api.v1.views import app_views
 from os import getenv
 from flask_cors import CORS
 
-if getenv("HBNB_HOST") is None:
-        HBNB_API_HOST = '0.0.0.0'
+if getenv("HBNB_API_HOST") is None:
+    HBNB_API_HOST = '0.0.0.0'
 else:
     HBNB_API_HOST = getenv("HBNB_API_HOST")
-if getenv("HBNB_PORT") is None:
+if getenv("HBNB_API_PORT") is None:
     HBNB_API_PORT = 5000
 else:
     HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
