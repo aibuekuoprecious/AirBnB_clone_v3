86fe6d957b24cad8c52736760800304f0aabaaa9
Aibueku O. Precious <aibuekuprecious@gmail.com>
2023-10-30 11:08:03 +0100

    AirBnB_clone_v3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,37 +1,24 @@
 #!/usr/bin/python3
-"""
-Flask route that returns json status response
-"""
+'''Contains the index view for the API.'''
+from flask import jsonify
 from api.v1.views import app_views
-from flask import jsonify, request
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
-def get_status():
-    """
-    Returns a JSON response with the status of the API
-    """
-    if request.method == 'GET':
-        resp = {"status": "OK"}
-        return jsonify(resp)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Returns JSON """
+    return jsonify(status="OK")
 
 
-@app_views.route('/stats', methods=['GET'])
-def get_stats():
-    """
-    Returns a JSON response with the count of all class objects
-    """
-    if request.method == 'GET':
-        response = {}
-        PLURALS = {
-            "Amenity": "amenities",
-            "City": "cities",
-            "Place": "places",
-            "Review": "reviews",
-            "State": "states",
-            "User": "users"
-        }
-        for key, value in PLURALS.items():
-            response[value] = storage.count(key)
-        return jsonify(response)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stat():
+    """returns the number of each objects by type"""
+    return jsonify(
+        amenities=storage.count('Amenity'),
+        cities=storage.count('City'),
+        places=storage.count('Place'),
+        reviews=storage.count('Review'),
+        states=storage.count('State'),
+        users=storage.count('User')
+    )
