47c37e44d25bfae1a4733f04d79dae22d40a27da
Aibueku O. Precious <aibuekuprecious@gmail.com>
2023-10-31 14:15:28 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -6,6 +6,8 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models import storage
+from models.place import Place
 
 
 class City(BaseModel, Base):
@@ -22,3 +24,28 @@ class City(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes city"""
         super().__init__(*args, **kwargs)
+
+    @property
+    def places(self):
+        """
+        Getter attribute to return the list of Place instances with city_id
+        equal to the current City.id
+        """
+        places_list = []
+        all_places = storage.all(Place)
+        for place in all_places.values():
+            if place.city_id == self.id:
+                places_list.append(place)
+        return places_list
+
+    def get(self, id):
+        """
+        Retrieve an object by ID.
+        """
+        return storage.get(City, id)
+
+    def count(self):
+        """
+        Count the number of City objects.
+        """
+        return len(storage.all(City))
